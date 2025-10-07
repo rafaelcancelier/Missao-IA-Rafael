28e3a00cac6b29d91a71864f77edcd89dcf3d2fb
rafaelcancelier <145573093+rafaelcancelier@users.noreply.github.com>
2025-10-07 12:17:45 -0300

    1234567890

--- main.js
+++ main.js
@@ -12,11 +12,11 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Fazer seu hobby em casa.",
-                afirmacao: "Você se sente mais confortável e relaxado ao praticar seu hobby no ambiente de casa",
+                afirmacao: "Você se sente mais confortável e relaxado ao praticar seu hobby no ambiente de casa"
             },
             {
                 texto: "Fazer seu hobby fora de casa",
-                afirmacao: "Você gosta de explorar novos lugares e aproveitar o ar livre ao realizar seu hobby",
+                afirmacao: "Você gosta de explorar novos lugares e aproveitar o ar livre ao realizar seu hobby"
             }
         ]
     },
@@ -25,11 +25,11 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Todos os dias ou quase todos os dias.",
-                afirmacao: "Você integra seu hobby na rotina diária para manter o equilíbrio e o prazer constante.",
+                afirmacao: "Você integra seu hobby na rotina diária para manter o equilíbrio e o prazer constante."
             },
             {
                 texto: "Algumas vezes por semana ou menos.",
-                afirmacao: "Você integra seu hobby na rotina diária para manter o equilíbrio e o prazer constante.",
+                afirmacao: "Você integra seu hobby na rotina diária para manter o equilíbrio e o prazer constante."
             }
         ]
     },
@@ -38,11 +38,11 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Hobbies criativos, como pintar ou escrever",
-                afirmacao: "Você gosta de expressar ideias e emoções através de criações artísticas e imaginativas.",
+                afirmacao: "Você gosta de expressar ideias e emoções através de criações artísticas e imaginativas."
             },
             {
                 texto: "Hobbies físicos, como correr ou dançar",
-                afirmacao: "Você gosta de expressar ideias e emoções através de criações artísticas e imaginativas.",
+                afirmacao: "Você gosta de expressar ideias e emoções através de criações artísticas e imaginativas."
             }
         ]
     },
@@ -51,11 +51,11 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Sim, gosto de fazer com amigos ou família.",
-                afirmacao: "Você encontra mais diversão ao conectar-se com outros e trocar experiências no hobby",
+                afirmacao: "Você encontra mais diversão ao conectar-se com outros e trocar experiências no hobby"
             },
             {
                 texto: "Não, prefiro praticar sozinho.",
-                afirmacao: "Você encontra mais diversão ao conectar-se com outros e trocar experiências no hobby",
+                afirmacao: "Você encontra mais diversão ao conectar-se com outros e trocar experiências no hobby"
             }
         ]
     },
@@ -64,11 +64,11 @@ const perguntas = [
         alternativas: [
             {
                 texto: "De manhã ou à tarde.",
-                afirmacao: "Você usa o hobby para energizar o dia e manter a produtividade alta.",
+                afirmacao: "Você usa o hobby para energizar o dia e manter a produtividade alta."
             },
             {
                 texto: "À noite ou antes de dormir.",
-                afirmacao: "Você usa o hobby para energizar o dia e manter a produtividade alta.",
+                afirmacao: "Você usa o hobby para energizar o dia e manter a produtividade alta."
             }
         ]
     },
